8c83d382339ddc40d0103a71708b1dda7b535460
ajsarsa <81232843+ajsarsa@users.noreply.github.com>
2024-04-29 20:12:20 +0200

    Create numdiff.py
    Formulas de derivacion numerica a 3 y 5 puntos

---
+++ 1 Basics/11 numder/numdiff.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+def snell_law(n1, n2, theta1):
+    '''
+    Ley de Snell, calcula el angulo de refracción
+
+    n1: refraction index
+    n2: refraction index
+    theta1: incidence angle
+
+    return theta2: refraction angle    
+    '''
+
+    stheta2 = n1*np.sin(theta1)/n2
+
+    return np.arcsin(stheta2
